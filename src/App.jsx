d844b5416a1ab4d660511e5d95fe80d59ebdef9d
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-09-06 09:11:17 +0600

    E-commerce website initial setup done with navbar

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Collection from './Pages/Collection';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Product from './Pages/Product'
+import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import PlaceOrder from './Pages/PlaceOrder';
+import Orders from './Pages/Orders';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/collection' element={<Collection></Collection>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/product/:productId' element={<Product></Product>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/placeOrder' element={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
