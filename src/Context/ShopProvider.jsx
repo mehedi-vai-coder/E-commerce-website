b38bc4ec116a1c28382b24ba90a4106b2412249f
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-09-17 10:22:56 +0600

    search bar implemented yet to fix logic

--- src/Context/ShopProvider.jsx
+++ src/Context/ShopProvider.jsx
@@ -1,13 +1,15 @@
-import React, { Children } from 'react';
+import React, { Children, useState } from 'react';
 import { ShopContext } from './ShopContext';
 import { products } from '../assets/assets';
 
 const ShopProvider = ({ children }) => {
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
 
     const shopInfo = {
-        products, delivery_fee, currency
+        products, delivery_fee, currency, search, setSearch, showSearch, setShowSearch
     }
     return (
         <ShopContext value={shopInfo}>
