db2f5d9971461d73820f05c487885bc0714c2b88
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-09-22 10:18:09 +0600

    Product details section ready

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,99 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext';
+import { assets } from '../assets/assets';
 
 const Product = () => {
-    return (
-        <div>
 
+    const { productId } = useParams();
+    const { products, currency } = useContext(ShopContext);
+    const [productData, setProductData] = useState(false);
+    const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
+
+    const fetchProductData = async () => {
+
+        products.map((item) => {
+            if (item._id === productId) {
+                setProductData(item)
+                setImage(item.image[0])
+                return null;
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetchProductData();
+    }, [productId, products])
+
+    return productData ? (
+        <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100 '>
+            {/* Product Data */}
+            <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+                {/* Product Images */}
+                <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row '>
+                    <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full '>
+                        {
+                            productData.image.map((item, index) => (
+                                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 cursor-pointer' alt="" />
+                            ))
+                        }
+                    </div>
+                    {/* Big Image  */}
+                    <div className='w-full sm:w-[80%]'>
+                        <img className='w-full h-auto' src={image} alt="" />
+                    </div>
+                </div>
+                {/* Product Information */}
+                <div className='flex-1'>
+                    <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+                    <div className='flex items-center gap-1 mt-2'>
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+                        <p className='pl-2'>(122)</p>
+                    </div>
+                    <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+                    <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+                    <div className='flex flex-col gap-4 my-8'>
+                        <p>Select Size</p>
+                        <div className='flex gap-2'>
+                            {
+                                productData.sizes.map((item, index) => (
+                                    <button onClick={() => setSize(item)} className={`border rounded border-gray-200 py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                                ))}
+                        </div>
+                    </div>
+                    <button className='bg-black rounded-2xl text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <hr className='mt-8 sm:w-4/5 border border-gray-200
+                    ' />
+                    <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                        <p>100% Orginal product.</p>
+                        <p>Cash on delivery is availabel on this product.</p>
+                        <p>East return and exchange policy within 7 days.</p>
+                    </div>
+                </div>
+            </div>
+            {/* ..........Description and Review section.......... */}
+            <div className='mt-20'>
+                <div className='flex '>
+                    <b className='border border-gray-200 px-5 py-3 text-sm'>
+                        Description
+                    </b>
+                    <p className='border border-gray-200 px-5 py-3 text-sm'>Reviews (122)</p>
+                </div>
+                {/* Description Box */}
+                <div className='flex flex-col gap-4 border border-gray-200 px-6 py-6 text-sm text-gray-500 mt-3 '>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni incidunt, ratione fuga corporis minus illo sapiente dolorum eos consectetur atque omnis, accusamus molestiae. Nemo, dignissimos!</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias dicta a illum dignissimos commodi adipisci voluptates sit ut sint. Sed, dolorem. Sit eligendi, iusto sequi quisquam harum eius corrupti est?</p>
+                </div>
+
+            </div>
         </div>
-    );
+    ) : <div className='opacity-0'> </div>
 };
 
 export default Product;
