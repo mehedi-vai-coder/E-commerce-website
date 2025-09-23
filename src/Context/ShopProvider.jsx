1209171299c32b5d411d06c8e743d3c4bb7c2d57
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-09-23 10:05:45 +0600

    add to cart and toast functionality

--- src/Context/ShopProvider.jsx
+++ src/Context/ShopProvider.jsx
@@ -1,15 +1,45 @@
-import React, { Children, useState } from 'react';
+import React, { Children, useEffect, useState } from 'react';
 import { ShopContext } from './ShopContext';
 import { products } from '../assets/assets';
+import { toast } from 'react-toastify';
 
 const ShopProvider = ({ children }) => {
     const currency = '$';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({})
+
+
+    const addToCart = async (itemId, size) => {
+
+        if (!size) {
+            toast.error('Select Product Size')
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData)
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
 
     const shopInfo = {
-        products, delivery_fee, currency, search, setSearch, showSearch, setShowSearch
+        products, delivery_fee, currency, search, setSearch, showSearch, setShowSearch, cartItems, addToCart,
     }
     return (
         <ShopContext value={shopInfo}>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProduct from '../Components/RelatedProduct';
 const Product = () => {
 
     const { productId } = useParams();
-    const { products, currency } = useContext(ShopContext);
+    const { products, currency, addToCart } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('');
     const [size, setSize] = useState('');
@@ -68,7 +68,7 @@ const Product = () => {
                                 ))}
                         </div>
                     </div>
-                    <button className='bg-black rounded-2xl text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <button onClick={() => addToCart(productData._id, size)} className='bg-black rounded-2xl text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
                     <hr className='mt-8 sm:w-4/5 border border-gray-200
                     ' />
                     <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
