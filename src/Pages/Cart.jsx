f7e3731f7eaee477a528a7a47973d245b9cabdd6
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-09-24 10:53:42 +0600

    add to card page ready yet to delete functionality

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../Context/ShopContext';
+import Title from './../Components/Title';
+import { assets } from '../assets/assets';
 
 const Cart = () => {
 
-    const { product, currency, cartItems } = useContext(ShopContext);
+    const { products, currency, cartItems } = useContext(ShopContext);
     const [cartData, setCartData] = useState([]);
 
     useEffect(() => {
@@ -19,11 +21,40 @@ const Cart = () => {
                 }
             }
         }
-        console.log(tempData)
+        setCartData(tempData)
     }, [cartItems])
 
     return (
-        <div>
+        <div className='border-t pt-14'>
+            <div className='text-2xl mb-3'>
+                <Title text1={'YOUR'} text2={'CART'} > </Title>
+            </div>
+
+            <div>
+                {
+                    cartData.map((item, index) => {
+                        const productData = products.find((product) => product._id === item._id);
+
+                        return (
+                            <div key={index} className='py-4 border-b border-t text-gray-700  grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2.5fr_0.5fr] items-center gap-4'>
+                                <div className='flex items-start gap-6'>
+                                    <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                                    <div>
+                                        <p className='text-sm sm:text-lg font-medium'>{productData.name}</p>
+                                        <div className='flex items-center gap-5 mt-2'>
+                                            <p>{currency}{productData.price}</p>
+                                            <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 ' type="number" min={1} defaultValue={item.quantity} />
+                                <img className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
+                            </div>
+                        )
+
+                    })
+                }
+            </div>
 
         </div>
     );
